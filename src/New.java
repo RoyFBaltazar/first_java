d46f0c14007d2721006c2c8e27a34cf76329ab37
RoyFBaltazar <94885352+RoyFBaltazar@users.noreply.github.com>
2022-04-25 14:28:01 -0400

    New math

---
+++ src/New.java
@@ -0,0 +1,14 @@
+
+public class Maths {
+    public int sum(int a, int b) {
+        int k = a + b;
+        return k;
+    }
+
+    public static void main(String [] args) {
+        Maths m = new Maths();
+        int result = m.sum(10, 20);
+        System.out.println("sum is " + result);
+        // prints - sum is 30
+    }
+}
