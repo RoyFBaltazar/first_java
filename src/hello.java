80f07647d4e0a44c281825f9c7ccc7073aea27ff
RoyFBaltazar <94885352+RoyFBaltazar@users.noreply.github.com>
2022-05-08 21:00:13 -0400

    Fixed synthax error

--- src/hello.java
+++ src/hello.java
@@ -7,32 +7,32 @@ public class hello {
         boolean sameNumberOfSongs = songsA == songsB;
         boolean differentLength = albumLengthA != albumLengthB;
 
-        System.out.println("hello world" + differentLength + sameNumberOfSongs);
+        System.out.println("hello world " + differentLength + sameNumberOfSongs);
     }
 }
-public class Store {
-    // instance fields
-    String productType;
-    double price;
-
-
-    // constructor method
-    public Store(String product, double initialPrice) {
-        productType = product;
-        price = initialPrice;
-    }
-
-    // increase price method
-    public void increasePrice(double priceToAdd){
-        double newPrice = price + priceToAdd;
-        price = newPrice;
-
-    }
-
-    // main method
-    public static void main(String[] args) {
-        Store lemonadeStand = new Store("Lemonade", 3.75);
-        lemonadeStand.increasePrice(1.5);
-        System.out.println(lemonadeStand.price);}
-    }
-
+//public class Store {
+//    // instance fields
+//    String productType;
+//    double price;
+//
+//
+//    // constructor method
+//    public Store(String product, double initialPrice) {
+//        productType = product;
+//        price = initialPrice;
+//    }
+//
+//    // increase price method
+//    public void increasePrice(double priceToAdd){
+//        double newPrice = price + priceToAdd;
+//        price = newPrice;
+//
+//    }
+//
+//    // main method
+//    public static void main(String[] args) {
+//        Store lemonadeStand = new Store("Lemonade", 3.75);
+//        lemonadeStand.increasePrice(1.5);
+//        System.out.println(lemonadeStand.price);}
+//    }
+//
